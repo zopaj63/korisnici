44353ce8f39f524fd32020f85cf611968619c883
zopaj63 <zoran.pajnic@gmail.com>
2023-09-27 15:16:05 +0200

    add kernel

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -21,6 +21,7 @@ class Kernel extends HttpKernel
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
+        "check.logged.user" =>\app\Http\Middleware\CheckLoggedUser::class,
     ];
 
     /**
